bb66a29d61c2dd7c7242be98c2af6833815abe21
beatrizamante <beatrizamante@hotmail.com>
2026-04-01 11:55:12 -0300

    Added http serving platform

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true }),
+  );
+  await app.listen(
+    Number(process.env.PORT ?? 3000),
+    process.env.ADDRESS ?? '127.0.0.1',
+  );
 }
-bootstrap();
+
+void bootstrap();
